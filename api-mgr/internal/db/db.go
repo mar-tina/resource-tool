b55b33b6f8fadb513d72f3235870cdf868348bee
martina khaemba <martina@martinas-MacBook-Air.local>
2021-01-25 23:39:54 +0300

    complete db functionality

--- api-mgr/internal/db/db.go
+++ api-mgr/internal/db/db.go
@@ -3,11 +3,17 @@ package db
 import "github.com/mar-tina/resource-tool/api-mgr/internal/schema"
 
 type Repository interface {
-	AddEndpointAncestors(collectionName, resourceName, serviceName string) error
 	// Names of the resource that's being accessed and the name of the service that is accessing it.
-	AddEndpointDescendants(collectionName, resourceName, serviceName string) error
 	StoreCollection(collection *schema.Collection) error
 	FetchCollection(collectionName string) (*schema.Collection, error)
+	CreateEnvironment(env *schema.Environment) error
+	FetchKeyInEnvironment(env, key string) (string, error)
+	FetchCollectionResource(servicename, resource string) (*schema.Request, error)
+	UpdateDescendants(parent, child, resource string) error
+	UpdateAncestors(child, parent, resource string) error
+	AllCollections(limit, skip int) ([]*schema.Collection, error)
+	UpdateEnvironment(env *schema.Environment) error
+	// AddEndpointAncestors(child, parent, resource string) error
 }
 
 var impl Repository
@@ -16,8 +22,8 @@ func SetImpl(repo Repository) {
 	impl = repo
 }
 
-func AddEndpointDescendants(collectionName, resourceName, serviceName string) error {
-	return impl.AddEndpointDescendants(collectionName, resourceName, serviceName)
+func CreateEnvironment(env *schema.Environment) error {
+	return impl.CreateEnvironment(env)
 }
 
 func StoreCollection(collection *schema.Collection) error {
@@ -28,6 +34,26 @@ func FetchCollection(collectionName string) (*schema.Collection, error) {
 	return impl.FetchCollection(collectionName)
 }
 
-func AddEndpointAncestors(collectionName, resourceName, serviceName string) error {
-	return impl.AddEndpointAncestors(collectionName, resourceName, serviceName)
+func FetchKeyInEnvironment(env, key string) (string, error) {
+	return impl.FetchKeyInEnvironment(env, key)
+}
+
+func FetchCollectionResource(servicename, resource string) (*schema.Request, error) {
+	return impl.FetchCollectionResource(servicename, resource)
+}
+
+func UpdateDescendants(parent, child, resource string) error {
+	return impl.UpdateDescendants(parent, child, resource)
+}
+
+func AllCollections(limit, skip int) ([]*schema.Collection, error) {
+	return impl.AllCollections(limit, skip)
+}
+
+func UpdateEnvironment(env *schema.Environment) error {
+	return impl.UpdateEnvironment(env)
+}
+
+func UpdateAncestors(child, parent, resource string) error {
+	return impl.UpdateAncestors(child, parent, resource)
 }
--- api-mgr/internal/db/storm.go
+++ api-mgr/internal/db/storm.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"errors"
 	"fmt"
 	"log"
 
@@ -24,79 +23,80 @@ func InitStormDB() *StormDB {
 	}
 }
 
-func (s *StormDB) AddEndpointDescendants(collectionName, resourceName, serviceName string) error {
-	coll := schema.Collection{}
-	//fetch the collection
-	err := s.db.One("Collection", collectionName, &coll)
-	if err != nil {
-		log.Printf("addEndpointDescendants failed %s", err)
-		return errors.New("addEndpointDescendants failed")
-	}
+// func (s *StormDB) AddEndpointDescendants(collectionName, resourceName, serviceName string) error {
+// 	coll := schema.Collection{}
+// 	//fetch the collection
+// 	err := s.db.One("Collection", collectionName, &coll)
+// 	if err != nil {
+// 		log.Printf("addEndpointDescendants failed %s", err)
+// 		return errors.New("addEndpointDescendants failed")
+// 	}
 
-	var serviceExists bool
-	// check if service already exists in the descendants list
-	for idx, srvc := range coll.Descendants {
-		//if exists update the list of resource names it is being consumed by
-		if srvc.Name == serviceName {
-			for _, rsc := range srvc.Resources {
-				if rsc != resourceName {
-					continue
-				}
-				srvc.Resources = append(srvc.Resources, resourceName)
-			}
-			serviceExists = true
-			coll.Descendants[idx] = srvc
-			s.db.Save(coll)
-			return nil
-		}
-	}
+// 	var serviceExists bool
+// 	// check if service already exists in the descendants list
+// 	for idx, srvc := range coll.Descendants {
+// 		//if exists update the list of resource names it is being consumed by
+// 		if srvc.Name == serviceName {
+// 			for _, rsc := range srvc.Resources {
+// 				if rsc != resourceName {
+// 					continue
+// 				}
+// 				srvc.Resources = append(srvc.Resources, resourceName)
+// 			}
+// 			serviceExists = true
+// 			coll.Descendants[idx] = srvc
+// 			s.db.Save(coll)
+// 			return nil
+// 		}
+// 	}
 
-	if !serviceExists {
-		srvc := schema.Service{}
-		srvc.Name = serviceName
-		srvc.Resources = append(srvc.Resources, resourceName)
-		coll.Descendants = append(coll.Descendants, srvc)
-	}
+// 	if !serviceExists {
+// 		srvc := schema.Service{}
+// 		srvc.Name = serviceName
+// 		srvc.Resources = append(srvc.Resources, resourceName)
+// 		coll.Descendants = append(coll.Descendants, srvc)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (s *StormDB) AddEndpointAncestors(collectionName, resourceName, serviceName string) error {
-	coll := schema.Collection{}
-	//fetch the collection
-	err := s.db.One("Collection", collectionName, &coll)
-	if err != nil {
-		log.Printf("addEndpointAncestors failed %s", err)
-		return errors.New("addEndpointAncestors failed")
-	}
+// func (s *StormDB) AddEndpointAncestors(collectionName, resourceName, serviceName string) error {
+// 	log.Printf
+// 	coll := schema.Collection{}
+// 	//fetch the collection
+// 	err := s.db.One("Collection", collectionName, &coll)
+// 	if err != nil {
+// 		log.Printf("addEndpointAncestors failed %s", err)
+// 		return errors.New("addEndpointAncestors failed")
+// 	}
 
-	var serviceExists bool
-	// check if service already exists in the descendants list
-	for idx, srvc := range coll.Ancestors {
-		//if exists update the list of resource names it is being consumed by
-		if srvc.Name == serviceName {
-			for _, rsc := range srvc.Resources {
-				if rsc != resourceName {
-					continue
-				}
-				srvc.Resources = append(srvc.Resources, resourceName)
-			}
-			serviceExists = true
-			coll.Ancestors[idx] = srvc
-			s.db.Save(coll)
-			return nil
-		}
-	}
+// 	var serviceExists bool
+// 	// check if service already exists in the descendants list
+// 	for idx, srvc := range coll.Ancestors {
+// 		//if exists update the list of resource names it is being consumed by
+// 		if srvc.Name == serviceName {
+// 			for _, rsc := range srvc.Resources {
+// 				if rsc != resourceName {
+// 					continue
+// 				}
+// 				srvc.Resources = append(srvc.Resources, resourceName)
+// 			}
+// 			serviceExists = true
+// 			coll.Ancestors[idx] = srvc
+// 			s.db.Save(coll)
+// 			return nil
+// 		}
+// 	}
 
-	if !serviceExists {
-		srvc := schema.Service{}
-		srvc.Name = serviceName
-		srvc.Resources = append(srvc.Resources, resourceName)
-		coll.Descendants = append(coll.Ancestors, srvc)
-	}
+// 	if !serviceExists {
+// 		srvc := schema.Service{}
+// 		srvc.Name = serviceName
+// 		srvc.Resources = append(srvc.Resources, resourceName)
+// 		coll.Descendants = append(coll.Ancestors, srvc)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 func (s *StormDB) StoreCollection(collection *schema.Collection) error {
 	err := s.db.Save(collection)
@@ -117,4 +117,186 @@ func (s *StormDB) FetchCollection(collectionName string) (*schema.Collection, er
 	}
 
 	return &coll, nil
+}
+
+//CreateEnvironment TODO: hash the values of the keys
+func (s *StormDB) CreateEnvironment(env *schema.Environment) error {
+	err := s.db.Save(env)
+	if err != nil {
+		log.Printf("createEnvironment failed %s", err)
+		return fmt.Errorf("createEnvironment failed %s", err)
+	}
+
+	return nil
+}
+
+func (s *StormDB) UpdateEnvironment(env *schema.Environment) error {
+	err := s.db.Update(&schema.Environment{ID: env.ID, Values: env.Values, Name: env.Name})
+	if err != nil {
+		log.Printf("updateEnvironment failed %s", err)
+		return fmt.Errorf("updateEnvironment failed %s", err)
+	}
+
+	return nil
+}
+
+func (s *StormDB) FetchKeyInEnvironment(env, key string) (string, error) {
+	var enviro schema.Environment
+	err := s.db.One("Name", env, &enviro)
+	if err != nil {
+		log.Printf("fetchKeyInEnvironment failed %s", err)
+		return "", fmt.Errorf("fetchKeyInEnvironment failed %s", err)
+	}
+
+	var res string
+
+	if &enviro != nil {
+		for _, el := range enviro.Values {
+			if el.Key == key {
+				res = el.Value
+				break
+			}
+		}
+	}
+
+	if res != "" {
+		return res, nil
+	}
+
+	return "", fmt.Errorf("could not find specified key")
+}
+
+func (s *StormDB) FetchCollectionResource(servicename, resource string) (*schema.Request, error) {
+	var coll schema.Collection
+	err := s.db.One("Name", servicename, &coll)
+	if err != nil {
+		log.Printf("failed to find specified resource %s", err)
+		return nil, fmt.Errorf("failed to find specified resource %s", err)
+	}
+
+	for _, item := range coll.Item {
+		for _, inner := range item.Item {
+			if inner.Name == resource {
+				return &inner.Request, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("could not find specified resource")
+}
+
+func (s *StormDB) UpdateDescendants(parent, child, resource string) error {
+	var coll schema.Collection
+	err := s.db.One("Name", parent, &coll)
+	if err != nil {
+		log.Printf("updateDescendants failed  %s", err)
+		return fmt.Errorf("updateDescendants %s", err)
+	}
+
+	var exists bool
+	var isChild bool
+
+	for _, desc := range coll.Descendants {
+		if desc.Name == child {
+			isChild = true
+			if isChild {
+				for _, item := range desc.Resources {
+					if resource == item {
+						exists = true
+						break
+					}
+				}
+			}
+
+			if !exists && isChild {
+				desc.Resources = append(desc.Resources, resource)
+				coll.Descendants = append(coll.Descendants, desc)
+				err := s.db.Update(&schema.Collection{ID: coll.ID, Descendants: coll.Descendants})
+				if err != nil {
+					log.Printf("db failed 1 %s", err)
+					return fmt.Errorf("updateDescendants %s", err)
+				}
+				return nil
+			}
+		}
+	}
+
+	if !exists && !isChild {
+		srvc := schema.Service{}
+		srvc.Name = child
+		srvc.Resources = append(srvc.Resources, resource)
+		coll.Descendants = append(coll.Descendants, srvc)
+		err := s.db.Update(&schema.Collection{ID: coll.ID, Descendants: coll.Descendants})
+		if err != nil {
+			log.Printf("db failed  %s", err)
+			return fmt.Errorf("updateDescendants here %s", err)
+		}
+
+	}
+
+	return nil
+}
+
+func (s *StormDB) UpdateAncestors(child, parent, resource string) error {
+	log.Printf("THE VALUES parent: %s child: %s", parent, child)
+	var coll schema.Collection
+	err := s.db.One("Name", child, &coll)
+	if err != nil {
+		log.Printf("UpdateAncestors failed  %s", err)
+		return fmt.Errorf("UpdateAncestors %s", err)
+	}
+
+	var exists bool
+	var isParent bool
+
+	for _, desc := range coll.Ancestors {
+		if desc.Name == parent {
+			isParent = true
+			if isParent {
+				for _, item := range desc.Resources {
+					if resource == item {
+						exists = true
+						break
+					}
+				}
+			}
+
+			if !exists && isParent {
+				desc.Resources = append(desc.Resources, resource)
+				coll.Descendants = append(coll.Ancestors, desc)
+				err := s.db.Update(&schema.Collection{ID: coll.ID, Ancestors: coll.Ancestors})
+				if err != nil {
+					log.Printf("db failed 1 %s", err)
+					return fmt.Errorf("UpdateAncestors %s", err)
+				}
+				return nil
+			}
+		}
+	}
+
+	if !exists && !isParent {
+		srvc := schema.Service{}
+		srvc.Name = child
+		srvc.Resources = append(srvc.Resources, resource)
+		coll.Descendants = append(coll.Ancestors, srvc)
+		err := s.db.Update(&schema.Collection{ID: coll.ID, Ancestors: coll.Ancestors})
+		if err != nil {
+			log.Printf("db failed  %s", err)
+			return fmt.Errorf("UpdateAncestors here %s", err)
+		}
+
+	}
+
+	return nil
+}
+
+func (s *StormDB) AllCollections(limit, skip int) ([]*schema.Collection, error) {
+	var colls []*schema.Collection
+	err := s.db.All(&colls, storm.Limit(limit), storm.Skip(skip), storm.Reverse())
+	if err != nil {
+		log.Printf("could not find collections")
+		return nil, fmt.Errorf("failed to fetch %s", err)
+	}
+
+	return colls, nil
 }
