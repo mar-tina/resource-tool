8e01c0ee9ad49379fed8abfce93a7d33bac7f887
martina khaemba <martina@martinas-MacBook-Air.local>
2021-01-25 23:41:28 +0300

    final schema

--- api-mgr/internal/schema/schema.go
+++ api-mgr/internal/schema/schema.go
@@ -1,15 +1,23 @@
 package schema
 
+type CollectionPayload struct {
+	Collection *Collection `json:"collection"`
+}
+
+type EnvPayload struct {
+	Environment *Environment `json:"environment"`
+}
+
 type Collection struct {
-	ID          string      `json:"id"`
-	Name        string      `json:"name"`
-	Owner       string      `json:"owner"`
-	UID         string      `json:"uid"`
-	Descendants []Service   `json:"descendants"`
-	Ancestors   []Service   `json:"ancestors"`
-	Resources   []Resource  `json:"resources"`
-	Info        Info        `json:"info"`
-	Item        []ItemEntry `json:"item"`
+	ID          string      `json:"id,omitempty"`
+	Name        string      `json:"name,omitempty"`
+	Owner       string      `json:"owner,omitempty"`
+	UID         string      `json:"uid,omitempty"`
+	Descendants []Service   `json:"descendants,omitempty"`
+	Ancestors   []Service   `json:"ancestors,omitempty"`
+	Resources   []Resource  `json:"resources,omitempty"`
+	Info        Info        `json:"info,omitempty"`
+	Item        []ItemEntry `json:"item,omitempty"`
 }
 
 type Service struct {
@@ -25,31 +33,31 @@ type Resource struct {
 }
 
 type ResourceCheckPayload struct {
-	CollectionName string `json:"collectionName"`
-	ResourceName   string `json:"resourceName"`
+	Collection string `json:"collection"`
+	Resource   string `json:"resource"`
 }
 
 type Info struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Schema      string `json:"schema"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	Schema      string `json:"schema,omitempty"`
 }
 
 type ItemEntry struct {
-	Item []NestedItem `json:"item"`
-	Name string       `json:"name"`
+	Item []NestedItem `json:"item,omitempty"`
+	Name string       `json:"name,omitempty"`
 }
 
 type NestedItem struct {
-	Name    string  `json:"name"`
-	Request Request `json:"request"`
+	Name    string  `json:"name,omitempty"`
+	Request Request `json:"request,omitempty"`
 }
 
 type Request struct {
-	URL    string   `json:"url"`
-	Method string   `json:"method"`
-	Header []Header `json:"header"`
-	Body   Body     `json:"body"`
+	URL    string   `json:"url,omitempty"`
+	Method string   `json:"method,omitempty"`
+	Header []Header `json:"header,omitempty"`
+	Body   Body     `json:"body,omitempty"`
 }
 
 type Header struct {
@@ -60,5 +68,47 @@ type Header struct {
 type Body struct {
 	Mode string `json:"mode"`
 	Raw  string `json:"raw"`
-	JSON string `json:"json"`
+}
+
+type Environment struct {
+	ID     string       `json:"id"`
+	Name   string       `json:"name"`
+	Values []*EnvValues `json:"values"`
+}
+
+type EnvValues struct {
+	Enabled bool   `json:"enabled"`
+	Key     string `json:"key,omitempty"`
+	Value   string `json:"value,omitempty"`
+	Type    string `json:"type,omitempty"`
+}
+
+type FetchResoure struct {
+	ServiceID string `json:"service_id"`
+	Key       string `json:"key"`
+	Resource  string `json:"resource"`
+}
+
+type FetchEnv struct {
+	ID string `json:"id"`
+}
+
+type FetchAllResult struct {
+	Environments []*Env `json:"environments"`
+}
+
+type Env struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Owner string `json:"owner"`
+	UID   string `json:"uid"`
+}
+
+type UpdateEnv struct {
+	Env Environment `json:"environment"`
+}
+
+type FetchAllCollections struct {
+	Limit int `json:"limit"`
+	Skip  int `json:"skip"`
 }
